0e69869f8d8273cabfbbdc19d89e1e7a0cb5ce74
飯田亘 <ht19a003@oecu.jp>
2021-11-20 11:40:13 +0900

    プログラム完成

--- src/sample01.cpp
+++ src/sample01.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 #define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。
+using namespace std;
+/*
+*is_prime
+*与えられた正の整数numが素数ならtrueを返すそうでなければfaulseを返す関数
+*/
+
+bool is_prime(int num) {
+	if (num == 1) {
+		return false;
+	}
+	for (int i = 2; i < num; i++) {
+		if (num%i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
 /* --------------------------------------------------------------- */
 /*
 * nth_prime
@@ -11,16 +28,30 @@
 * ファイルを作成せよ。
 */
 /* -------------------------------------------------------------- */
+
 int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
-	int num = a;
-	for (int i = 0; i < 100; i++) {
-		num += d;
-		std::cout << i << std::endl;
+	int count = 0;
+	int e = a;
+	while (e < CPP2_PRIME_UPPER_LIMIT) {
+		if (e == 1) {
+			exit(-1);
+		}
+		if (is_prime(e)) {
+			count++;
+		}
+		if (count == n) {
+			break;
+			count << 0;
+		}
+		e += d;
 	}
-	return 0;
+
+	return e;
 }
 int main() {
-    std::cout << nth_prime(367, 186, 151) << std::endl;
-    // 以下、同様に、入出力例通りになるか確認せよ。
-    return 0;
+	std::cout << nth_prime(367, 186, 151) << std::endl;
+	// 以下、同様に、入出力例通りになるか確認せよ。
+	cin.get();
+	cin.get();
+	return 0;
 }
